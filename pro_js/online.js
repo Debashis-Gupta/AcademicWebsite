3315d5838a2df85565f1cb36003efe2653dc4d0e
Debashis-Gupta <debashisguptapapai@gmail.com>
2021-09-02 20:49:42 +0600

    adding certifications and mobileview update

---
+++ pro_js/online.js
@@ -0,0 +1,9 @@
+function dataScience(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('dataScience');
+    
+    popup.classList.toggle('active');
+  }
+
+  

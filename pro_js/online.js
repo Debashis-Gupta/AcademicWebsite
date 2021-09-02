2a024a5030e68ff6ebb597114edffc6267c55007
Debashis-Gupta <debashisguptapapai@gmail.com>
2021-09-02 21:33:34 +0600

    Update of honors and mobile view

--- pro_js/online.js
+++ pro_js/online.js
@@ -6,4 +6,12 @@ function dataScience(){
     popup.classList.toggle('active');
   }
 
+  function award(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('award');
+    
+    popup.classList.toggle('active');
+  }
+
   
